deb22f711d71353a83cf38fb4782cca143f11f74
Alexander von Trostorff <axel1200@users.noreply.github.com>
2018-11-28 12:35:11 +0100

    Lombok
    Fixes #4

--- src/main/java/de/vontrostorff/kivsproject/parsing/dtos/PingGroup.java
+++ src/main/java/de/vontrostorff/kivsproject/parsing/dtos/PingGroup.java
@@ -1,11 +1,13 @@
 package de.vontrostorff.kivsproject.parsing.dtos;
 
 
-import java.time.OffsetDateTime;
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Data
 public class PingGroup {
     private final List<Ping> pings= new ArrayList<>(11);
     private float min;
@@ -15,55 +17,4 @@ public class PingGroup {
     private int transmitted;
     private int received;
 
-    public void setMin(float min) {
-        this.min = min;
-    }
-
-    public void setMax(float max) {
-        this.max = max;
-    }
-
-    public void setAverage(float average) {
-        this.average = average;
-    }
-
-    public void setStart(Date start) {
-        this.start = start;
-    }
-
-    public List<Ping> getPings() {
-        return pings;
-    }
-
-    public int getTransmitted() {
-        return transmitted;
-    }
-
-    public void setTransmitted(int transmitted) {
-        this.transmitted = transmitted;
-    }
-
-    public int getReceived() {
-        return received;
-    }
-
-    public void setReceived(int received) {
-        this.received = received;
-    }
-
-    public float getMin() {
-        return min;
-    }
-
-    public float getMax() {
-        return max;
-    }
-
-    public float getAverage() {
-        return average;
-    }
-
-    public Date getStart() {
-        return start;
-    }
 }
